41d87a3ce2e6fa6bbd1dd26b0fbc033003568a77
ro77en <lucas.mdantas@icloud.com>
2025-05-05 23:12:44 -0300

    feat: add highlight directive

---
+++ src/app/shared/directives/highlight.directive.ts
@@ -0,0 +1,28 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]',
+})
+export class HighlightDirective {
+  @Input() highlightColor = '#f0f8ff'; // Light blue default
+  private originalColor = '';
+
+  constructor(private el: ElementRef) {}
+
+  ngOnInit() {
+    this.originalColor = this.el.nativeElement.style.backgroundColor || '';
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight(this.highlightColor);
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight(this.originalColor);
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+    this.el.nativeElement.style.transition = 'background-color 0.3s';
+  }
+}
