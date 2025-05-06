6781787ccc579790abee3c6eda0b25e8b96e3180
ro77en <lucas.mdantas@icloud.com>
2025-05-05 23:41:23 -0300

    feat: home component

---
+++ src/app/features/home/home/home.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+import { AuthService } from '../../../core/services/auth/auth.service';
+import { User } from '../../../core/model/user.model';
+
+@Component({
+  selector: 'app-home',
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent implements OnInit {
+  currentUser$!: Observable<User | null>;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.currentUser$ = this.authService.currentUser$;
+  }
+}
