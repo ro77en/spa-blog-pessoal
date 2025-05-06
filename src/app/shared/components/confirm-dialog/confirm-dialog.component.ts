3bef3fa6bb46f6ff71d8fc01d9227c74d5bac2c7
ro77en <lucas.mdantas@icloud.com>
2025-05-05 23:11:12 -0300

    feat: add confirm dialog component

---
+++ src/app/shared/components/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,28 @@
+import { Component, Inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  MatDialogModule,
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+
+interface DialogData {
+  title: string;
+  message: string;
+  confirmText: string;
+  cancelText: string;
+}
+
+@Component({
+  selector: 'app-confirm-dialog',
+  imports: [CommonModule, MatDialogModule, MatButtonModule],
+  templateUrl: './confirm-dialog.component.html',
+  styleUrl: './confirm-dialog.component.scss',
+})
+export class ConfirmDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
+}
