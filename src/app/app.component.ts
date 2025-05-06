f61cba5de58b53ca315bdc8b27cf06bf3dc24c3c
ro77en <lucas.mdantas@icloud.com>
2025-05-06 00:05:14 -0300

    feat: use router module in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'frontend-blog-pessoal';
+  title = 'spa-blog-pessoal';
 }
