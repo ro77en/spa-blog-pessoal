e30f6354c8faae333126e34d8e7753864b53696b
ro77en <lucas.mdantas@icloud.com>
2025-05-13 13:07:30 -0300

    feat: bind login form html to typescript

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -6,9 +8,13 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 
+import { AuthService } from '../../auth/auth.service';
+
 @Component({
   selector: 'app-login-form',
   imports: [
+    CommonModule,
+    FormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatCheckboxModule,
@@ -18,4 +24,33 @@ import { MatCardModule } from '@angular/material/card';
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss',
 })
-export class LoginFormComponent {}
+export class LoginFormComponent {
+  username: string = '';
+  password: string = '';
+  termsCheckbox: boolean = false;
+  errorMsg: string = '';
+  isSubmitting: boolean = false;
+
+  constructor(private authService: AuthService) {}
+
+  onSubmit(): void {
+    this.isSubmitting = true;
+
+    if (!this.username || !this.password) {
+      this.errorMsg = 'Por favor, preencha todos os campos';
+      return;
+    }
+
+    const loginData = {
+      username: this.username,
+      password: this.password,
+    };
+
+    this.authService.login(loginData).subscribe({
+      next: () => console.log('success!'),
+      error: () => {
+        this.errorMsg = 'Erro de autenticação. Verifique suas credenciais';
+      },
+    });
+  }
+}
