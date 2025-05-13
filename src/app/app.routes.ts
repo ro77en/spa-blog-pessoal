3218e2fa9f1a0c0411209e385e0ec5a26545bfed
ro77en <lucas.mdantas@icloud.com>
2025-05-13 18:30:20 -0300

    feat: add logged in guard to protect login page when user is autenticated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AuthGuard } from './auth/auth.guard';
+import { LoggedInGuard } from './auth/logged-in.guard';
 
 export const routes: Routes = [
   {
     path: '',
     component: LoginPageComponent,
+    canActivate: [LoggedInGuard],
   },
   {
     path: 'home',
@@ -14,7 +16,7 @@ export const routes: Routes = [
     canActivate: [AuthGuard],
   },
   {
-    path: '*',
+    path: '**',
     redirectTo: '/home',
   },
 ];
