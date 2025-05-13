52fc222d16331e993f3296abc5a2cb043ea2ec64
ro77en <lucas.mdantas@icloud.com>
2025-05-13 18:27:58 -0300

    feat: show navbar when user is authenticated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AuthService } from './auth/auth.service';
+import { CommonModule } from '@angular/common';
+import { StorageService } from './services/storage.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'spa-blog-pessoal';
+
+  constructor(private authService: AuthService) {}
+
+  get currentUsername(): string | null {
+    return this.authService.currentUser()?.username ?? null;
+  }
 }
